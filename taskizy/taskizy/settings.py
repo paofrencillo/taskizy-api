e554c1e4bd7f809f9447c652034971c9c87d03da
paofrencillo <paolofrencillocodes@gmail.com>
2023-10-25 16:42:04 +0800

    configs for deployment

--- taskizy/taskizy/settings.py
+++ taskizy/taskizy/settings.py
@@ -29,8 +29,8 @@ DEBUG = False
 ALLOWED_HOSTS = ["*"]
 
 CORS_ALLOWED_ORIGINS = [
-    "http://127.0.0.1:5173",
-    "http://localhost:5173",
+    "http://127.0.0.1:5173/",
+    "http://localhost:5173/",
 ]
 
 # Email Setup
@@ -207,7 +207,7 @@ DJOSER = {
 # Cloudinary settings
 CLOUDINARY_URL = env("CLOUDINARY_URL")
 cloudinary.config(
-    cloud_name="YOUR_CLOUD_NAME",
-    api_key="YOUR_API_KEY",
-    api_secret="YOUR_API_SECRET",
+    cloud_name="CLOUDINARY_CLOUD_NAME",
+    api_key="CLOUDINARY_API_KEY",
+    api_secret="CLOUDINARY_API_SECRET",
 )
