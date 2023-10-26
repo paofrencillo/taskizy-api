f23e5bb345034b14c9e11fd68bf5e68c2293cf98
paofrencillo <paolofrencillocodes@gmail.com>
2023-10-26 12:08:30 +0800

    some changes in settings py

--- taskizy/taskizy/settings.py
+++ taskizy/taskizy/settings.py
@@ -19,7 +19,7 @@ environ.Env.read_env(BASE_DIR / ".env")
 SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env("DEBUG")
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
