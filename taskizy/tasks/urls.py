837ba44397bacb20c20b3b74e9b92e8d7f20e3c4
paofrencillo <paolofrencillocodes@gmail.com>
2023-10-27 21:30:30 +0800

    changes in paths

--- taskizy/tasks/urls.py
+++ taskizy/tasks/urls.py
@@ -3,22 +3,22 @@ from .views import *
 
 urlpatterns = [
     path(
-        "task/room<int:room_id>/create/",
+        "task/room/<int:room_id>/create/",
         TasksListCreateView.as_view(),
         name="task-create",
     ),
     path(
-        "task/room<int:room_id>/task<int:task_id>/mark-done/",
+        "task/room/<int:room_id>/task/<int:task_id>/mark-done/",
         TaskRetrieveUpdateDestroyView.as_view(),
         name="task-complete",
     ),
     path(
-        "task/room<int:room_id>/task<int:task_id>/delete/",
+        "task/room/<int:room_id>/task/<int:task_id>/delete/",
         TaskRetrieveUpdateDestroyView.as_view(),
         name="task-delete",
     ),
     path(
-        "tasks/user<int:pk>/",
+        "tasks/user/<int:pk>/",
         UserTasksListCreateView.as_view(),
         name="user-tasks",
     ),
